7eaee01377f972d1b4b2f0ab0299b34bc55ad0f0
Youssef Mohamed <yussf.mohamed.o@gmail.com>
2022-08-15 01:45:35 +0200

    Add linear time solution

---
+++ Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Problem number: 121
+ * Difficulty: Easy
+ * 
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+int maxProfit(int[] prices)
+{
+	int pricesSize = prices.Length + 1;
+
+	int[] table = new int[prices.Length + 1];
+
+	table[0] = 0;
+	table[1] = 0;
+
+	if (prices.Length >= 2)
+	{
+		table[2] = prices[1] > prices[0] ? prices[1] - prices[0] : 0;
+		int tempMin = Math.Min(prices[0], prices[1]);
+
+		for (int i = 3; i <= prices.Length; i++)
+		{
+			tempMin = Math.Min(tempMin, prices[i - 1]);
+			table[i] = Math.Max(table[i - 1], prices[i - 1] - tempMin);
+		}
+	}
+
+	return table[pricesSize];
+}
