a348787e32d32dceeb1793d0f2dcce247debb821
Youssef Mohamed <yussf.mohamed.o@gmail.com>
2022-08-15 02:09:28 +0200

    Add linear time solution

---
+++ Counting Bits/Counting Bits/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Problem number: 338
+ * Difficulty: Easy
+ * 
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+int[] countBits(int n)
+{
+	int[] table = new int [n +1];
+	
+	if (n == 0)
+	{
+		return table;
+	}
+	else if (n == 1)
+	{
+		table[0] = 0;
+		table[1] = 1;
+		return table;
+	}
+	else if (n == 2)
+	{
+		table[0] = 0;
+		table[1] = 1;
+		table[2] = 1;
+		return table;
+	}
+	
+	table[0] = 0;
+	table[1] = 1;
+	table[2] = 1;
+	
+	if (n >= 3)
+	{
+		for (int i = 3; i <= n; i++)
+		{
+			if (i % 2 == 0)
+				table[i] = table[i / 2];
+	
+			else if (i % 2 != 0)
+				table[i] = table[i / 2] + table[2];
+		}
+	}
+	
+	return table;
+}
